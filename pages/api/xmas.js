23fb3068026bd0aec4c39e8e6cae05953e453c8c
Andrey Atapin <a.atapin@gmail.com>
2022-12-15 18:32:24 +0500

    answers traversal

--- pages/api/xmas.js
+++ pages/api/xmas.js
@@ -1,10 +1,15 @@
 export default function handler(req, res) {
-  if (req.method === 'POST' && req.headers['secret'] == process.env.WEBHOOK_KEY) {
+  if (req.method === 'POST') {
     console.log("POST request received");
-    req.body['answer']['answers'].forEach(a => {
-      console.log("Answer: ", a);
-    });
-    res.status(200).json({ message: 'OK' });
+    if (req.headers['secret'] === process.env.WEBHOOK_KEY) {
+      console.log("Webhook key is valid");
+      req.body['answer']['answers'].forEach(a => {
+        console.log("Answer: ", a);
+      });
+      res.status(200).json({ message: 'OK' });
+    } else {
+      res.status(403);
+    }
   } else {
     res.status(415);
   }
